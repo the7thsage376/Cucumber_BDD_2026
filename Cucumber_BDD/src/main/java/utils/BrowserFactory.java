7d0a699aa7397715e52de932e93d9b1b6c4f6bd8
Lerato Moloi <moloil405@gmail.com>
2026-06-04 22:15:41 +0200

    Created README and browser Factory

--- Cucumber_BDD/src/main/java/utils/BrowserFactory.java
+++ Cucumber_BDD/src/main/java/utils/BrowserFactory.java
@@ -1,6 +1,34 @@
 package utils;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 public class BrowserFactory {
+    public static WebDriver driver;
+
+    public static WebDriver startBrowser(String browserChoice, String url) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        EdgeOptions edgeOptions = new EdgeOptions();
+
+        if (browserChoice.equalsIgnoreCase("ChRoMe")){
+            chromeOptions.addArguments("--headless");
+
+            driver = new ChromeDriver(chromeOptions);
+
+        } else if (browserChoice.equalsIgnoreCase(("EdgE"))){
+            edgeOptions.addArguments("--headless");
+
+            driver = new EdgeDriver(edgeOptions);
+
+        }
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+        driver.get(url);
+        return driver;
 
 
+    }
 }
